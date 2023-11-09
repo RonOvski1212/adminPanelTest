076d063f045372467707deb55839d14b422001f7
RonOvski1212 <245785gio@gmail.com>
2023-11-09 16:25:14 +0400

    optimisation

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,8 @@
-import { createStore, combineReducers, compose, applyMiddleware } from "redux";
-import ReduxThunk from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
 import heroes from "../reducers/heroes";
 import filters from "../reducers/filters";
 
-const stribgMiddleware = () => (next) => (action) => {
+const stringMiddleware = () => (next) => (action) => {
   if (typeof action === "string") {
     return next({
       type: action,
@@ -12,12 +11,19 @@ const stribgMiddleware = () => (next) => (action) => {
   return next(action);
 };
 
-const store = createStore(
-  combineReducers({ heroes, filters }),
-  compose(
-    applyMiddleware(ReduxThunk, stribgMiddleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store = configureStore({
+  reducer: { heroes, filters },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(stringMiddleware),
+  devTools: process.env.NODE_ENV !== "production",
+});
+
+// const store = createStore(
+//   combineReducers({ heroes, filters }),
+//   compose(
+//     applyMiddleware(ReduxThunk, stringMiddleware),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// );
 
 export default store;
